f01dd97aa0f7d5eca66e583f52316c4d18a06036
Eric Laursen <ericjlaursen@gmail.com>
2020-01-16 21:27:30 -0500

    gats config

---
+++ gatsby-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  siteMetadata: {
+    title: 'A Gatsby Project',
+    description:
+      'A site built from the inspiration of Jason Lengstorf',
+  },
+  plugins: ['gatsby-plugin-emotion', 'gatsby-plugin-react-helmet'],
+};
