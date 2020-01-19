ffa3b2c0c8208a9fed4bcad3d4afc0e0c9588e5f
Eric Laursen <ericjlaursen@gmail.com>
2020-01-18 20:35:26 -0500

    missing word

--- src/hooks/use-sitemetadata.js
+++ src/hooks/use-sitemetadata.js
@@ -1,8 +1,14 @@
 import { useStaticQuery, graphql } from 'gatsby';
 
+
+/*
+  gets 'meta-data' 
+  from gatsby-config file
+*/
+
 const useSiteMetadata = () => {
   const data = useStaticQuery(graphql`
-    {
+    query {
       site {
         siteMetadata {
           title
