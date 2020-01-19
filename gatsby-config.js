8ea31ec55a48161b4221b9b0327b90a346c2d4c1
Eric Laursen <ericjlaursen@gmail.com>
2020-01-18 20:46:16 -0500

    gatsby-plugin-mdx config

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,10 +8,10 @@ module.exports = {
   	'gatsby-plugin-emotion', 
   	'gatsby-plugin-react-helmet',
   	{
-  		resolve: 'gatsby-mdx',
+  		resolve: 'gatsby-plugin-mdx',
   		options: {
   			// use the header/footer as shared layouts
-  			default: require.resolve('./src/components/layout.js')
+  			defaultLayouts: require.resolve('./src/components/layout.js')
   		}
   	}
   ],
